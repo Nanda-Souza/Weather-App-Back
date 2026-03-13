0170e274ef08cca80574d19dca7a80a79aa2b3a0
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-03-13 08:42:29 -0300

    chore: Config Inicial do Projeto

---
+++ src/main/java/com/db/weather_app_back/WeatherAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.db.weather_app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/weather_app_back/WeatherAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.weather_app_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
