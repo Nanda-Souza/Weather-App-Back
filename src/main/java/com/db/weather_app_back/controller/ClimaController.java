1d55c0f6b230d2aee4d75389662d84d2d0f6e053
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-03-17 08:30:14 -0300

    feat: Configuração do Swagger

--- src/main/java/com/db/weather_app_back/controller/ClimaController.java
+++ src/main/java/com/db/weather_app_back/controller/ClimaController.java
@@ -3,6 +3,9 @@ package com.db.weather_app_back.controller;
 import com.db.weather_app_back.domain.dto.ClimaRequest;
 import com.db.weather_app_back.domain.dto.ClimaResponse;
 import com.db.weather_app_back.domain.service.ClimaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,12 @@ public class ClimaController {
 
     public ClimaController(ClimaService climaService) {this.climaService = climaService;}
 
+    @Operation(description = "Cadastra um dado meteorológico")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cria dado meteorológico com base nos dados informados"),
+            @ApiResponse(responseCode = "409", description = "Não cria o dado meteorológico pois já existe dado para a data/cidade informada"),
+            @ApiResponse(responseCode = "400", description = "Não cria o dado meteorológico pois os dados informados são inválidos")
+    })
     @PostMapping("/cadastrar")
     public ResponseEntity<ClimaResponse> cadastrarDadoMeteorologico(
             @RequestBody  @Valid ClimaRequest climaRequest
@@ -34,6 +43,11 @@ public class ClimaController {
         return ResponseEntity.created(location).body(salvo);
     }
 
+    @Operation(description = "Busca um dado meteorológico por por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o meteorológico com base no Id informado"),
+            @ApiResponse(responseCode = "404", description = "Não encontrou o meteorológico pelo id informado")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ClimaResponse> buscarDadoMeteorologicoPorId(
             @PathVariable Long id
